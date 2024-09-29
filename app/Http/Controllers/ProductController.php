4d87e7f842c46a09ce955c7df820e1e122d2ac12
conn-do <kondo@japaan.jp>
2024-09-29 19:28:33 +0900

    商品一覧

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::orderBy('id', 'desc')->get();
+
+        return view('top-page', [
+            'products' => $products,
+        ]);
+    }
+}
